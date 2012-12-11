bba9fe313c19ce3ee5d853f180ce9f85ca21aa6f
Ben Draut <drautb@gmail.com>
2012-12-11 13:49:58 -0700

    Changed tab size to four spaces

--- note-taker/Form1.cs
+++ note-taker/Form1.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Diagnostics;
+using System.Runtime.InteropServices;
 
 namespace note_taker
 {
@@ -25,8 +26,8 @@ namespace note_taker
         private NoteListSerializer serializer = new NoteListSerializer();
 
         // Component References
-        DataGridViewColumn previewTextCol;
-        DataGridViewColumn modifiedTextCol;
+        private DataGridViewColumn previewTextCol;
+        private DataGridViewColumn modifiedTextCol;
 
         /**
          * Constants
@@ -34,12 +35,19 @@ namespace note_taker
         private const String NOTES_FILENAME = "notes.bin";
         private const String NOTES_BACKUP_FILENAME = "notes.bak";
 
+        private const int EM_SETTABSTOPS = 0x00CB;
+        private const int FONT_TO_TEMPLATE_WIDTH_RATIO = 4;
+        private const int TAB_WIDTH = 4;
+
         /**
          * Constructor
          */
         public MainForm()
         {
             InitializeComponent();
+
+            SetTabWidth(noteTextArea, TAB_WIDTH);
+
             InitDataGrid();
 
             LoadNotes();
@@ -51,6 +59,23 @@ namespace note_taker
             /*
             Test();
             /**/
+        }
+
+        /**
+         * Subroutine to change the tab size
+         * 
+         * Based on this page:
+         * http://stackoverflow.com/questions/1298406/how-to-set-the-tab-width-in-a-windows-forms-textbox-control
+         * 
+         */
+        [DllImport("User32.dll", CharSet = CharSet.Auto)]
+        public static extern IntPtr SendMessage(IntPtr h, int msg, int wParam, int[] lParam);
+
+        public static void SetTabWidth(TextBox textBox, int tabWidth)
+        {
+            Graphics g = textBox.CreateGraphics();
+
+            SendMessage(textBox.Handle, EM_SETTABSTOPS, 1, new int[] { tabWidth * FONT_TO_TEMPLATE_WIDTH_RATIO });
         }
 
         /**
